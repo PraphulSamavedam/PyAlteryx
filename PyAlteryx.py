6de7ee5d3a194396f5fdcc5499b7d17bda19d871
Monkey306 <51016667+PraphulSamavedam@users.noreply.github.com>
2021-05-06 20:55:54 +0530

    added steps for oauth_signature generation

--- PyAlteryx.py
+++ PyAlteryx.py
@@ -1,17 +1,20 @@
+import hmac
 from time import time
 from random import choice
 from string import ascii_letters
 import requests
+from urllib.parse import urlencode, quote
+from hashlib import sha1
 
 class AlteryxGalleryConnection:
-    def __init__(self, api_key:str, shared_secret:str, api_url:str):
+    def __init__(self, api_key:str, secret:str, api_url:str):
         """ Parameters required for the Gallery connection  while initializing
          :arg api_key: str "Obtain this information under your account settings
-         :arg shared_secret: str "Obtain this information under your account settings
+         :arg secret: str "Obtain this information under your account settings
          :arg api_url: str "This is the url fo the api
         """
         self.api_key = api_key
-        self.api_shared_secret = shared_secret
+        self.api_secret = secret
         if api_url.endswith('/api/v1/'):
             self.api_url = api_url
         elif api_url.endswith('/api/v1'):
@@ -25,14 +28,14 @@ class AlteryxGalleryConnection:
                   f"{api_url.endswith(start=len(api_url)-8)}")
             raise ValueError
 
-    def __getOAuthparams(self) -> dict:
+    def getOAuthparams(self, http_method:str, url:str) -> dict:
         params = dict()
         params['oauth_signature_method'] = 'HMAC-SHA1'
         params['oauth_version'] = '1.0'
         params['oauth_consumer_key'] = self.api_key
         params['oauth_timestamp'] = str(int(time()))
         params['oauth_nonce'] = "".join(choice(ascii_letters) for x in range(5))
-        params['signature'] = self.generatesignature()
+        params['signature'] = self._generate_signature(http_method=http_method, url= url, params=params)
         return  params
 
     def getWorkFlowsUnderSubscription(self):
@@ -40,7 +43,15 @@ class AlteryxGalleryConnection:
         url = self.api_url + "workflows/subscription/"
         params = self.__getOAuthparams()
         output = requests.get(url=url, params=params)
+        return output
 
-    def generatesignature(self):
-        pass
+    def _generate_signature(self, http_method:str, url:str,params:dict):
+        byte_encoded_secret = bytearray(self.api_secret,encoding='UTF-8')
+        raw_d = urlencode(params)
+        encoded_header = urlencode(params, safe='~', quote_via=quote)
+        encoded_url = urlencode(params, safe='~', quote_via=quote)
+        encoded_msg =  "&".join(http_method.upper(),encoded_url,encoded_header)
+        hmac_encoded = hmac.new(key=byte_encoded_secret,msg=encoded_msg,digestmod=sha1)
+        return hmac_encoded.digest().encode('base64').rstrip('\n')
+
 
